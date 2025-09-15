87d96ec7851e7290a64b975e0f81b706f29ea2e3
wjr21 <wjr21@sussex.ac.uk>
2025-09-15 15:00:02 +0100

    Some easier formatting in the setup script

--- setup.sh
+++ setup.sh
@@ -6,17 +6,23 @@
 REPO_DIR=$1
 echo "Cloning the ExGal-SPH-workshop repository into $REPO_DIR"
 git clone https://github.com/WillJRoper/ExGal-SPH-workshop.git $REPO_DIR
+echo "\n"
 
 # Load the necessary modules
 echo "Loading necessary modules..."
 echo "Running: module load intel_comp/2024.2.0 compiler-rt tbb compiler mpi ucx/1.17.0 parallel_hdf5/1.14.4 fftw/3.3.10 parmetis/4.0.3-64bit gsl/2.8 python/3.12.4"
 module load intel_comp/2024.2.0 compiler-rt tbb compiler mpi ucx/1.17.0 parallel_hdf5/1.14.4 fftw/3.3.10 parmetis/4.0.3-64bit gsl/2.8 python/3.12.4
 module list
+echo "\n"
 
 # Clone SWIFT
 SWIFT_DIR=$REPO_DIR/swiftsim
 echo "Cloning SWIFT into $SWIFT_DIR"
 git clone https://gitlab.cosma.dur.ac.uk/swift/swiftsim.git $SWIFT_DIR
+cd $SWIFT_DIR
+echo ""
+./autogen.sh
+echo "\n"
 
 # Download EAGLE data dependencies incase we use them
 echo "Downloading EAGLE data dependencies..."
