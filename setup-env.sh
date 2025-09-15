2b73320b8d58f70d1bab16501ca1ca74bf896cf5
wjr21 <wjr21@sussex.ac.uk>
2025-09-15 17:26:05 +0100

    Clean up

--- setup-env.sh
+++ setup-env.sh
@@ -27,4 +27,4 @@ source $REPO_DIR/venv/bin/activate
 # background reporting the url to connect to
 NOTEBOOK_DIR=$REPO_DIR/notebooks
 echo "Starting a jupyter notebook server in $NOTEBOOK_DIR"
-jupyter notebook --ip='*' --port=8888 --no-browser --allow-root --notebook-dir=$NOTEBOOK_DIR
+jupyter notebook --ip='*' --port=8888 --no-browser --notebook-dir=$NOTEBOOK_DIR
