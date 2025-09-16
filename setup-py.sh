8434415b93b3e3695dea3f82db9ae08893bfdb7e
wjr21 <wjr21@sussex.ac.uk>
2025-09-16 17:30:21 +0100

    Typo

--- setup-py.sh
+++ setup-py.sh
@@ -3,8 +3,7 @@
 # Set up the python environment
 module load python/3.12.4
 python -m venv sph-env
-source sph-venv/bin/activate
+source sph-env/bin/activate
 pip install --upgrade pip
-pip install -r $REPO_DIR/requirements.txt
+pip install -r requirements.txt
 deactivate
-echo ""
